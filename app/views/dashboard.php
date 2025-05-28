8cabd9a31e2afca35c29a50c66e5ef01b9399a3a
hercease <hercease001@gmail.com>
2025-05-28 14:22:16 +0100

    update

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -658,8 +658,7 @@
                   </div>
                 </div>
               </div>
-              <?php print_r($countdownlinespercentage); ?>
-              <?php print_r($globalDownlinespercentage); ?>
+              
               <?php if($countdownlinespercentage === 100 && $globalDownlinespercentage === 100){  ?>
                 <a id="<?php echo $userInfo['id'] ?>" class="btn btn-secondary btn-shadow reward">Claim Reward</a>
               <?php } ?>
