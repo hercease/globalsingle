890951e0eefaa6ce9517313e144001a5689526f6
hercease <hercease001@gmail.com>
2025-06-01 15:20:51 +0100

    update

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ workbox.setConfig({
 });
 
 // Cache name
-const CACHE_NAME = 'globalsingle-cache-v1';
+const CACHE_NAME = 'globalsingle';
 workbox.core.setCacheNameDetails({
   prefix: CACHE_NAME,
 });
